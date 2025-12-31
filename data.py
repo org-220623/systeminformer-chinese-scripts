ba0cbcb07f73c17a97c68657ca100901558e0a36
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-01 07:27:23 +0800

    Update data.py

--- data.py
+++ data.py
@@ -837,6 +837,11 @@ TRANSLATION_DATA: list[tuple[str,      str,  data_list_type,   raw_data_list_typ
         "Run as admin task UI access": "以管理员身份运行任务 UI 访问权限",
         "Run as admin task UI access: %!STATUS!": "以管理员身份运行任务 UI 访问权限: %!STATUS!",
     }, []),
+    (f"{CONST_PATH_SYSTEM_INFORMER_SRC}/affinity.c", "utf-8", {
+        "Unable to change affinity of process %lu": "无法更改进程 %lu 的处理器相关性",
+        "Unable to change affinity of thread %lu": "无法更改线程 %lu 的处理器相关性",
+        
+    }, []),
     #################################################################################
     # System Informer PEView Tool source files: CONST_PATH_PEVIEW_TOOL_SRC
     (f"{CONST_PATH_PEVIEW_TOOL_SRC}/peview.rc", "utf-8", {
