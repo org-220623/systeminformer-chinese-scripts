2fb2e0559b1f360d11bbc35009c4f9ab9586d8cc
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-01 21:37:55 +0800

    update data

--- push-system-informer-project.cmd
+++ push-system-informer-project.cmd
@@ -3,10 +3,12 @@ git commit -m "update data"
 git push origin
 git push gitee
 git push github
+git push gitea
 cd ..
 git add .python.patch
 git commit -m "update submodule"
 git push origin
 git push gitee
 git push gitlab
+git push gitea
 git restore .
