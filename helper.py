d438593da7643f979fb1d27a28bc7f44f6302674
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-07 21:46:05 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -4,9 +4,13 @@ data = {
     "push": "push.bat",
     "mrnd": "main_nodebug.bat",
     "gaph": "gitea-push.bat",
+    "burl": ["build-release.bat", "restore.bat"],
+    "rste": "restore.bat",
     # number
+    "0": ["build-release.bat", "restore.bat"],
     "1": "push.bat",
     "2": "gitea-push.bat",
+    "3": "restore.bat",
 }
 
 def print_info():
@@ -22,6 +26,15 @@ while True:
         continue
     try:
         file = data[command]
-        os.system("misc\\Batch\\" + file)
+        if isinstance(file, str):
+            os.system("misc\\Batch\\" + file)
+        elif isinstance(file, list):
+            for i in file:
+                if isinstance(i, str):
+                    os.system("misc\\Batch\\" + i)
+                else:
+                    print("Unknown type. [0x1]")
+        else:
+            print("Unknown type. [0x2]")
     except Exception as error:
         print("Unknown command.")
