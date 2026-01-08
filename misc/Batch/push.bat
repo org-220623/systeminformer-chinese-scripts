00af20ee633f1b685891800c9250e132fbf9754b
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-08 22:29:29 +0800

    update data

--- misc/Batch/push.bat
+++ misc/Batch/push.bat
@@ -7,8 +7,8 @@ git push gitea
 cd ..
 git add .python.patch
 git commit -m "update submodule"
-git push origin
-git push gitee
-git push gitlab
-git push gitea
+git push --all origin
+git push --all gitee
+git push --all gitlab
+git push --all gitea
 git restore .
