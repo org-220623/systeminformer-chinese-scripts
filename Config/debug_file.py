fedda37ae772a998139d26a9cf689ff26e058e74
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-12 20:56:48 +0800

    update data

--- Config/debug_file.py
+++ Config/debug_file.py
@@ -1,4 +1,4 @@
 # 当开启调试模式时，将只处理 debug_file 变量指定的文件。
 from Config.const_values import *
 
-DEBUG_FILE = f"{CONST_PATH_SYSTEM_INFORMER_SRC}/sysscmem.c"
+DEBUG_FILE = f"{CONST_PATH_SYSTEM_INFORMER_SRC}/thrdlist.c"
