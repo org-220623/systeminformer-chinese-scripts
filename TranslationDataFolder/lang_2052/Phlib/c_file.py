2e95f0c86318642b616e5e47bffd4ebfeb44cbb6
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-17 17:18:21 +0800

    update data

--- TranslationDataFolder/lang_2052/Phlib/c_file.py
+++ TranslationDataFolder/lang_2052/Phlib/c_file.py
@@ -324,7 +324,7 @@ DATA: TranslationDataType = [
         "Disabled": "禁用",
         "Ignore": "忽略",
         "Normal": "正常",
-        "Severe": "严格", # todo 严格 or 严重？
+        "Severe": "严重", # todo 严格 or 严重？
         "Critical": "关键",
         # Line 254
     }, [
