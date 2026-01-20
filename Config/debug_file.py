54501d2d145be3a9676a1f3ae59407b229a85bed
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-20 18:02:34 +0800

    update data

--- Config/debug_file.py
+++ Config/debug_file.py
@@ -1,4 +1,4 @@
 # 当开启调试模式时，将只处理 debug_file 变量指定的文件。
 from Config.const_values import *
 
-DEBUG_FILE = f"{CONST_PATH_PLUGIN_EXTENDEDTOOLS}/objprp.c"
+DEBUG_FILE = f"{CONST_PATH_PLUGIN_EXTENDEDTOOLS}/pooldialogbig.c"
