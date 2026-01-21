993addd895f97d249e075f1ac77fbc5c68f6ff21
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-21 17:37:44 +0800

    update data

--- TranslationDataFolder/lang_2052/Plugins/ToolStatus/c_file.py
+++ TranslationDataFolder/lang_2052/Plugins/ToolStatus/c_file.py
@@ -2,5 +2,13 @@ from Config.const_values import CONST_PATH_PLUGIN_TOOLSTATUS
 from Config.static_data_type import TranslationDataType
 
 DATA: TranslationDataType = [
-
+    (f"{CONST_PATH_PLUGIN_TOOLSTATUS}/customizetb.c", "utf-8", {
+        "No text labels": "无文本标签",
+        "Selective text": "可选择文本",
+        "Show text labels": "显示文本标签",
+        "Always show": "总是显示",
+        "Hide when inactive (Ctrl+K)": "不活动时隐藏 (Ctrl+K)",
+        "Auto-hide": "自动隐藏",
+        "Separator": "分隔符",
+    }, []),
 ]
