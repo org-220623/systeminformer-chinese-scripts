6dbe5d74c7c3844195d70b1f0962a35a90ac59c1
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-25 18:11:48 +0800

    update data

--- TranslationDataFolder/lang_2052/Plugins/NetworkTools/resource_file.py
+++ TranslationDataFolder/lang_2052/Plugins/NetworkTools/resource_file.py
@@ -13,7 +13,7 @@ DATA: TranslationDataType = [
         "Max Tracert Hops:": "最大跟踪路由跳数:",
         "Enable extended TCP statistics": "启用扩展 TCP 统计信息",
         "MaxMind GeoLite Account ID:": "MaxMind GeoLite 帐户 ID:",
-        "Open GeoIP.conf": "打开 GeoIP.conf",
+        "Open GeoIP.conf": "导入 GeoIP.conf",
         "Change": "更改",
         "Note: You can also download GeoLite updates from the Main menu > Tools > Network Tools > Update GeoLite...": "注意：您也可以从主菜单 > 工具 > 网络工具 > 更新 GeoLite 下载 GeoLite 更新...",
         "MaxMind GeoLite License Key:": "MaxMind GeoLite 许可证密钥:",
@@ -39,5 +39,7 @@ DATA: TranslationDataType = [
         "Paste the license key below:": "在下方粘贴许可证密钥:",
         r'Checkout <a href=""https://support.maxmind.com/hc/en-us/articles/4407111582235-Generate-a-License-Key"">How-to-generate-a-license-key</a>':
             r'查看 <a href=""https://support.maxmind.com/hc/en-us/articles/4407111582235-Generate-a-License-Key"">如何生成许可证密钥</a>'
-    }, []),
+    }, [
+        ('"MaxMind GeoLite License Key:",IDC_STATIC,16,116,97,8', '"MaxMind GeoLite License Key:",IDC_STATIC,16,116,130,8'), 
+    ]),
 ]
