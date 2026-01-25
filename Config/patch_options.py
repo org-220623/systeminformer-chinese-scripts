808ca65c32ed46f0a14fcf3508dff6ec8548291c
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-25 16:09:41 +0800

    update data

--- Config/patch_options.py
+++ Config/patch_options.py
@@ -1,3 +1,3 @@
 from Config.static_data_type import TranslateDotNetCounters
 
-OPTIONS_TRANSLATE_DOTNET_COUNTERS: TranslateDotNetCounters = TranslateDotNetCounters.PartialTranslateAddHeader
+OPTIONS_TRANSLATE_DOTNET_COUNTERS: TranslateDotNetCounters = TranslateDotNetCounters.AddHeader
