4f3f93a5912dba5a627218993886989ec121ed3a
Zhang Yixuan <192800783+anonymous9075331734@users.noreply.github.com>
2026-01-28 22:12:35 +0800

    update data

--- TranslationDataFolder/lang_2052/Plugins/NetworkTools/resource_file.py
+++ TranslationDataFolder/lang_2052/Plugins/NetworkTools/resource_file.py
@@ -15,7 +15,7 @@ DATA: TranslationDataType = [
         "MaxMind GeoLite Account ID:": "MaxMind GeoLite 帐户 ID:",
         "Open GeoIP.conf": "导入 GeoIP.conf",
         "Change": "更改",
-        "Note: You can also download GeoLite updates from the Main menu > Tools > Network Tools > Update GeoLite...": "注意: 您也可以从主菜单 > 工具 > 网络工具 > 更新 GeoLite 下载 GeoLite 更新...",
+        "Note: You can also download GeoLite updates from the Main menu > Tools > Network Tools > Update GeoLite...": "注意: 您也可以从主菜单 > 工具 > 网络工具 > 更新 GeoLite 下载 GeoLite 更新",
         "MaxMind GeoLite License Key:": "MaxMind GeoLite 许可证密钥:",
         "Download update": "下载更新",
         "GeoLite Update": "GeoLite 更新",
